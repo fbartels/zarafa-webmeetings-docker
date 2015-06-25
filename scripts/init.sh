2550deb5428bd1e73017c7012d11630bf098c229
Felix Bartels <felix@host-consultants.de>
2015-06-25 07:28:15 +0200

    add customer ssl certificates and a boilerplate to init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+
+if [ -z $ZARAFA_HOST ]; then
+	cat << EOF
+
+ERROR:
+This image needs a set of environment variables specified. The example configuration can be found at https://github.com/fbartels/zarafa-webmeetings-docker/blob/master/env.conf.
+
+With this file you can then start the container in the following way:
+
+docker run -it --env-file=env.conf \
+-p 10080:80 -p 10443:443 \
+zarafa-webmeetings-docker
+
+EOF
+	exit 1
+fi
+
+cat << EOF
+
+This Docker container helps you run/test Zarafa Web Meetings. All your data remains on your current server and this container only serves as a (disposable) frontend.
+
+All you have to do is set ZARAFA_HOST to point to your current server in the accompanying env.conf. For added security you can also customise the individual secrects and add your own ssl certificates.
+
+Zarafa Web Meetings and a custom Zarafa WebApp will be available shortly via https://IP-of-your-docker-host:10443 (regular http requests will be redirected).
+
+EOF
+
 # replace url of Zarafa host
 sed -i -e 's,file:///var/run/zarafa,'${ZARAFA_HOST}',g' /etc/zarafa/webapp/config.php
 
@@ -12,13 +39,16 @@ sed -i -e 's,tne-default-encryption-block-key,'${WEBMEETINGS_ENCRYPTION_SECRET}'
 sed -i -e 's,some-secret-do-not-keep,'${WEBMEETINGS_SHARED_SECRET}',g' /etc/zarafa/webmeetings.cfg
 sed -i -e 's,/webapp/plugins/spreedwebrtc/php/AngularPluginWrapper.php,/plugins/spreedwebrtc/php/AngularPluginWrapper.php,g' /etc/zarafa/webmeetings.cfg
 
-# 
+if [[ ! -z $SSL_CERTIFICATE ]] || [[ ! -z $SSL_CERTIFICATE_KEY ]]; then
+	echo "using certificate specified in env.conf"
+	echo -e "$SSL_CERTIFICATE" > /etc/ssl/certs/ssl-cert-snakeoil.pem
+	echo -e "$SSL_CERTIFICATE_KEY" > /etc/ssl/private/ssl-cert-snakeoil.key
+fi
 
 service php5-fpm start
 service zarafa-presence start
 service zarafa-webmeetings start
 
 # exec CMD
-echo ">> exec docker CMD"
-echo "$@"
+echo "Starting $@ .."
 exec "$@"
