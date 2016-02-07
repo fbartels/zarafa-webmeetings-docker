841a1d85d5aa368e32adebe3124b70a1f4ae35ab
Felix Bartels <felix@host-consultants.de>
2016-02-07 12:09:39 +0100

    make testing easier with build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-sudo docker build -t zarafa-webmeetings-docker .
+sudo docker build --no-cache -t zarafa-webmeetings-docker .
 
-echo
-read -p "Run this build?? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	sudo docker run -it --env-file=env.conf \
-	-p 10080:10080 -p 10443:10443 \
-	zarafa-webmeetings-docker
-fi
+sudo docker run -it --rm -v /usr/local/bin:/target -v /etc/z-container:/conf zarafa-webmeetings-docker instl
