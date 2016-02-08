ab69f15f9eded3b9b8d12ce336f0a73f1187d0e7
Felix Bartels <felix@host-consultants.de>
2016-02-08 21:24:03 +0100

    name container and remove it before rebuilding. also remove --no-cache

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo docker build --no-cache -t zarafa-webmeetings-docker .
+sudo docker rm zarafa-webmeetings
 
-sudo docker run -it --rm -v /usr/local/bin:/target -v /etc/z-container:/conf zarafa-webmeetings-docker instl
+sudo docker build -t zarafa-webmeetings-docker .
+
+sudo docker run --name=zarafa-webmeetings -it --rm -v /usr/local/bin:/target -v /etc/z-container:/conf zarafa-webmeetings-docker instl
